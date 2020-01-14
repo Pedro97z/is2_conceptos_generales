5816b5df5c886023e7f629126aa9c1d331201e61
W3411 <W3411@lab.aca>
2020-01-14 17:07:16 -0500

    ejemplos 1

---
+++ conceptos.ts
@@ -0,0 +1,28 @@
+interface Pedido{
+    nombre : string
+    cantidad : number
+}
+
+class Cliente{
+    realizarPedido(mozo : Mozo, pedido : Pedido){
+        mozo.recibirPedido(pedido) //momento donde se origina el mensaje
+    }
+}
+
+class Mozo{
+    recibirPedido(pedido : Pedido) : void{
+        console.log("Se recibio el pedido")
+    }
+}
+
+let main = () => {
+    let pepito = new Cliente()
+    let mozo : Mozo = new Mozo()
+    let pedido : Pedido = {
+        nombre : "Ceviche",
+        cantidad : 2
+    }
+
+    pepito.realizarPedido(mozo, pedido)
+}
+main() //ejecutando la funcion main definida anteriormente
